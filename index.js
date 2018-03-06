a923cdd9939d807db580a8d203c0c4379ae6d631
Florisvan der Breggen <florisvanderbreggen@gmail.com>
2018-03-06 17:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(holiday, name) {
 	return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(days, holiday) {
-	return `It’s ${days} days until, ${holiday}!`;
+function holidayCountdown(holiday, days) {
+	return `It’s ${days} days until ${holiday}!`;
 }
