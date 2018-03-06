f0bf68fe5e62d0600ffca0ff425a412f19d3b64d
Florisvan der Breggen <florisvanderbreggen@gmail.com>
2018-03-06 16:55:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidaysTo(name) {
+	return `Happy holidays, $(name)!`
+}
+
+function happyHolidaysTo(holiday, name) {
+	return `Happy $(holiday), $(name)!`
+}
+
+function holidayCountdown(days, holiday) {
+	return `It’s $(days) days until, $(holiday)!`
+} 
